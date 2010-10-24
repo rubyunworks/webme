b7d3eb903208fa1c6d208919a24931599ada5f0a
7rans <transfire@gmail.com>
2010-10-24 15:47:33 -0400

    make main part of Sowfile a Sow extension

---
+++ meta/data.rb
@@ -0,0 +1,30 @@
+module WebMe
+
+  def self.__DIR__
+    File.dirname(__FILE__)
+  end
+
+  def self.package
+    @package ||= (
+      require 'yaml'
+      YAML.load(File.new(__DIR__ + '/package'))
+    )
+  end
+
+  def self.profile
+    @profile ||= (
+      require 'yaml'
+      YAML.load(File.new(__DIR__ + '/profile'))
+    )
+  end
+
+  def self.const_missing(name)
+    key = name.to_s.downcase
+    package[key] || profile[key] || super(name)
+  end
+
+end
+
+# becuase Ruby 1.8~ gets in the way
+Object.__send__(:remove_const, :VERSION) if Object.const_defined?(:VERSION)
+
